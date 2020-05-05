1882749e5c1cecf32f84cf4b70ae11f3a2b31637
Jérémy Gfeller <36196039+JeremyGfeller@users.noreply.github.com>
2020-05-05 12:29:26 +0200

    Rename the about page by login

---
+++ src/views/Login.vue
@@ -0,0 +1,45 @@
+<template>
+  <div class="about">
+    <h1>This is an about page</h1>
+
+    <button v-if="!loggedIn" v-on:click="login()">connexion</button>
+    <button v-else v-on:click="logout()">déconnexion</button>
+  </div>
+</template>
+
+<script>
+/* eslint-disable */
+import { mapActions, mapGetters } from 'vuex';
+
+export default {
+  name: 'login',
+  data() {
+    return {};
+  },
+  props: ['loading'],
+  computed: {
+    ...mapActions('authentication', [
+      'initGapi'
+    ]),
+    ...mapGetters('authentication', [
+      'loggedIn'
+    ])
+  },
+  methods: {
+    ...mapGetters('authentication', [
+      'loggedIn'
+    ]),
+    ...mapActions('authentication', [
+      'signIn',
+      'signOut',
+      'isSignedIn'
+    ]),
+    login() {
+      this.signIn();
+    },
+    logout() {
+      this.signOut();
+    }
+  }
+};
+</script>
