78f0935cf314584c4c4bf6af2fe0a7c68aa066bb
Jérémy Gfeller <36196039+JeremyGfeller@users.noreply.github.com>
2020-05-05 12:29:13 +0200

    Create URLs for testing if the authentication works

--- src/router/index.js
+++ src/router/index.js
@@ -1,22 +1,28 @@
+/* eslint-disable */
 import Vue from 'vue'
 import VueRouter from 'vue-router'
 import Home from '../views/Home.vue'
+import Login from '../views/Login.vue'
+import store from "../store";
 
 Vue.use(VueRouter)
 
   const routes = [
   {
-    path: '/',
-    name: 'Home',
-    component: Home
+    path: '/login',
+    name: 'Login',
+    component: Login,
+    meta: {
+      requiresAuth: false
+    }
   },
   {
-    path: '/about',
-    name: 'About',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
+    path: '/home',
+    name: 'Home',
+    component: Home,
+    meta: {
+      requiresAuth: true
+    }
   }
 ]
 
@@ -24,6 +30,22 @@ const router = new VueRouter({
   mode: 'history',
   base: process.env.BASE_URL,
   routes
+})
+
+router.beforeEach((to, from, next) => {
+  if (to.matched.some(record => record.meta.requiresAuth)) {
+    store.dispatch("authentication/isSignedIn").then(isSignedIn => {
+      if (!isSignedIn) {
+        next({
+          path: '/login',
+        })
+      } else {
+        next()
+      }
+    })
+  } else {
+    next() // assurez vous de toujours appeler `next()` !
+  }
 })
 
 export default router
