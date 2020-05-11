70248d63bf29b4c49e812e33c25bd268ee873455
Jérémy Gfeller <36196039+JeremyGfeller@users.noreply.github.com>
2020-05-11 16:49:57 +0200

    Create persisted values between pages

--- src/store/index.js
+++ src/store/index.js
@@ -2,10 +2,12 @@ import Vue from 'vue'
 import Vuex from 'vuex'
 import authentication from './authentication';
 import authorization from './authorization';
+import createPersistedState from 'vuex-persistedstate';
 
 Vue.use(Vuex)
 
 export default new Vuex.Store({
+  namespaced: true,
   state: {
   },
   mutations: {
@@ -15,5 +17,14 @@ export default new Vuex.Store({
   modules: {
     authentication,
     authorization
-  }
+  },
+  plugins: [
+    createPersistedState({
+      paths: [
+        'authentication.profile',
+        'authentication.loggedIn',
+        'authorization.mainTableData'
+      ]
+    })
+  ]
 })
