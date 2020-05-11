9e755d6727d3799611e824fa119eba67428fa596
Jérémy Gfeller <36196039+JeremyGfeller@users.noreply.github.com>
2020-05-11 16:49:35 +0200

    Get and update a cell in the spreadhseet #7 and #9

--- src/store/authorization.js
+++ src/store/authorization.js
@@ -8,18 +8,13 @@ export default {
     spreadsheetId: process.env.VUE_APP_SPREADSHEET_ID,
     mainTableData: null,
     loading: false,
-    mainRangeData: null
   },
   getters: {
     tableData: state => state.mainTableData,
-    rangeData: state => state.mainRangeData,
   },
   mutations: {
     assignTableData(state, tableData) {
       state.mainTableData = tableData;
-    },
-    assignRangeData(state, rangeData) {
-      state.mainRangeData = rangeData;
     },
     loading(state, loadingState) {
       state.logging = loadingState;
@@ -33,32 +28,51 @@ export default {
       var ranges = [
         "saisie-2020!A1:AV"
       ];
-      gapi.client.load('https://sheets.googleapis.com/$discovery/rest?version=v4').then(() => {
-        gapi.client.sheets.spreadsheets.values.get({
-          spreadsheetId: state.spreadsheetId,
-          range: ranges
-        }).then((response) => {
-          console.log(response.result)
-          var array = []
-          response.result.values.forEach((element, index) => {
-            if(index > 0){
-              if (element[2] == 'Gilles') {
-                element.push((index+1))
-                array.push(element)
-              }
+      gapi.client.sheets.spreadsheets.values.get({
+        spreadsheetId: state.spreadsheetId,
+        range: ranges
+      }).then((response) => {
+        console.log(response.result)
+        var array = []
+        response.result.values.forEach((element, index) => {
+          if(index > 0){
+            if (element[3] == 'gilles@antistatique.net') {
+              // dire pourquoi le + 1
+              element.push((index+1))
+              array.push(element)
             }
-          })
-          // console.log(array)
-          array.find(element => {
-            if (element[1] == moment().isoWeek()) {
-              commit('assignTableData', element);
-              return true;
-            }
-          })
-          if (response.status !== 200) {
-            return false;
           }
-        });
+        })
+        console.log(array)
+        array.find(element => {
+          if (element[1] == moment().isoWeek()) {
+            commit('assignTableData', element);
+            return true;
+          }
+        })
+        if (response.status !== 200) {
+          return false;
+        }
+      });
+    },
+    updateSheet({ state, commit }, payload) {
+      console.log(payload)
+      var values = [
+        [
+          payload.value
+        ],
+      ];
+      var body = {
+        values: values
+      };
+      gapi.client.sheets.spreadsheets.values.update({
+        spreadsheetId: state.spreadsheetId,
+        range: 'saisie-2020!' + payload.column + payload.lines,
+        valueInputOption: 'USER_ENTERED',
+        resource: body
+      }).then((response) => {
+        var result = response.result;
+        console.log(`${result.updatedCells} cells updated.`);
       });
     }
   }
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,106 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+   <div class="home">
+      <table class=" table-data entry-hours">
+        <thead>
+          <tr>
+            <th colspan="2">Matin</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td>Début</td>
+            <td>
+              <input type="time" v-model="amBegin" v-on:keyup.enter="sendAmBegin(amBegin)">
+            </td>
+          </tr>
+          <tr>
+            <td>Fin</td>
+            <td><input type="time" v-model="amEnd" v-on:keyup.enter="sendAmEnd(amEnd)"></td>
+          </tr>
+        </tbody>
+      </table>
+      <table class=" table-data entry-hours">
+        <thead>
+          <tr>
+            <th colspan="2">Après-midi</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td>Début</td>
+            <td>
+              <input type="time" v-model="pmBegin" v-on:keyup.enter="sendPmBegin(pmBegin)">
+            </td>
+          </tr>
+          <tr>
+            <td>Fin</td>
+            <td><input type="time" v-model="pmEnd" v-on:keyup.enter="sendPmEnd(pmEnd)"></td>
+          </tr>
+        </tbody>
+      </table>
+
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+/* eslint-disable */
+import { mapActions, mapGetters } from 'vuex';
 
 export default {
   name: 'Home',
-  components: {
-    HelloWorld
+  data: () => ({
+    amBegin : null,
+    amEnd : null,
+    pmBegin : null,
+    pmEnd : null,
+  }),
+  methods: {
+    ...mapActions('authorization', [
+      'updateSheet'
+    ]),
+    sendAmBegin(amBegin){
+      let payload = {
+        'value': amBegin,
+        'column': 'I',
+        'lines': this.tableData[47]
+      }
+      this.updateSheet(payload)
+    },
+    sendAmEnd(amEnd){
+      let payload = {
+        'value': amEnd,
+        'column': 'J',
+        'lines': this.tableData[47]
+      }
+      this.updateSheet(payload)
+    },
+    sendPmBegin(pmBegin){
+      let payload = {
+        'value': pmBegin,
+        'column': 'K',
+        'lines': this.tableData[47]
+      }
+      this.updateSheet(payload)
+    },
+    sendPmEnd(pmEnd){
+      let payload = {
+        'value': pmEnd,
+        'column': 'L',
+        'lines': this.tableData[47]
+      }
+      this.updateSheet(payload)
+    },
+  computed: {
+    ...mapGetters('authorization', [
+      'tableData'
+    ])
+  },
+  mounted() {
+    return [
+      this.amBegin = this.tableData[8],
+      this.amEnd = this.tableData[9],
+      this.pmBegin = this.tableData[10],
+      this.pmEnd = this.tableData[11]
+    ]
   }
 }
 </script>
