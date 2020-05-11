538a0b014de43f547b28d8b5e8d114fbfe7309b8
Jérémy Gfeller <36196039+JeremyGfeller@users.noreply.github.com>
2020-05-11 11:18:10 +0200

    Get values from the spreadsheet for the user Gilles (for the moment)

--- src/store/authorization.js
+++ src/store/authorization.js
@@ -1,5 +1,6 @@
 /* global gapi */
 /* eslint-disable */
+import * as moment from 'moment'
 
 export default {
   namespaced: true,
@@ -25,27 +26,40 @@ export default {
     },
   },
   actions: {
-    querySheet({
+    sheetQuery({
       state,
       commit
     }) {
       var ranges = [
         "saisie-2020!A1:AV"
       ];
-      gapi.client.load('https://sheets.googleapis.com/$discovery/rest?version=v4').then((resp) => {
-        console.log(resp)
+      gapi.client.load('https://sheets.googleapis.com/$discovery/rest?version=v4').then(() => {
         gapi.client.sheets.spreadsheets.values.get({
           spreadsheetId: state.spreadsheetId,
           range: ranges
         }).then((response) => {
           console.log(response.result)
+          var array = []
+          response.result.values.forEach((element, index) => {
+            if(index > 0){
+              if (element[2] == 'Gilles') {
+                element.push((index+1))
+                array.push(element)
+              }
+            }
+          })
+          // console.log(array)
+          array.find(element => {
+            if (element[1] == moment().isoWeek()) {
+              commit('assignTableData', element);
+              return true;
+            }
+          })
           if (response.status !== 200) {
             return false;
           }
-          // commit('assignTableData', array);
-          return true;
         });
       });
-    },
+    }
   }
 };
