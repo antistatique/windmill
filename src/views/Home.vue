18bb54d5c1d0b002475be0b9fd9258cf4a983993
Jérémy Gfeller <36196039+JeremyGfeller@users.noreply.github.com>
2020-05-12 10:11:09 +0200

    Update values for each day of the week

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -44,6 +44,7 @@
 <script>
 /* eslint-disable */
 import { mapActions, mapGetters } from 'vuex';
+import moment from 'moment'
 
 export default {
   name: 'Home',
@@ -52,40 +53,95 @@ export default {
     amEnd : null,
     pmBegin : null,
     pmEnd : null,
+    lines: null,
+    currentDay: null,
+    days: {
+      Monday: {
+        amBegin: 'I',
+        amBeginIndex: 8,
+        amEnd: 'J',
+        amEndIndex: 9,
+        pmBegin: 'K',
+        pmBeginIndex: 10,
+        pmEnd: 'L',
+        pmEndIndex: 11,
+      },
+      Tuesday: {
+        amBegin: 'Q',
+        amBeginIndex: (8+8),
+        amEnd: 'R',
+        amEndIndex: (9+8),
+        pmBegin: 'S',
+        pmBeginIndex: (10+8),
+        pmEnd: 'T',
+        pmEndIndex: (11+8),
+      },
+      Wednesday: {
+        amBegin: 'Y',
+        amBeginIndex: (8+16),
+        amEnd: 'Z',
+        amEndIndex: (8+16),
+        pmBegin: 'AA',
+        pmBeginIndex: (8+16),
+        pmEnd: 'AB',
+        pmEndIndex: (8+16),
+      },
+      Thursday: {
+        amBegin: 'AG',
+        amBeginIndex: (8+24),
+        amEnd: 'AH',
+        amEndIndex: (8+24),
+        pmBegin: 'AI',
+        pmBeginIndex: (8+24),
+        pmEnd: 'AJ',
+        pmEndIndex: (8+24),
+      },
+      Friday: {
+        amBegin: 'AO',
+        amBeginIndex: (8+32),
+        amEnd: 'AP',
+        amEndIndex: (8+32),
+        pmBegin: 'AQ',
+        pmBeginIndex: (8+32),
+        pmEnd: 'AR',
+        pmEndIndex: (8+32),
+      }
+    }
   }),
   methods: {
     ...mapActions('authorization', [
+      'getSheet',
       'updateSheet'
     ]),
     sendAmBegin(amBegin){
       let payload = {
         'value': amBegin,
-        'column': 'I',
-        'lines': this.tableData[47]
+        'column': this.days[this.currentDay].amBegin,
+        'lines': this.lines
       }
       this.updateSheet(payload)
     },
     sendAmEnd(amEnd){
       let payload = {
         'value': amEnd,
-        'column': 'J',
-        'lines': this.tableData[47]
+        'column': this.days[this.currentDay].amEnd,
+        'lines': this.lines
       }
       this.updateSheet(payload)
     },
     sendPmBegin(pmBegin){
       let payload = {
         'value': pmBegin,
-        'column': 'K',
-        'lines': this.tableData[47]
+        'column': this.days[this.currentDay].pmBegin,
+        'lines': this.lines
       }
       this.updateSheet(payload)
     },
     sendPmEnd(pmEnd){
       let payload = {
         'value': pmEnd,
-        'column': 'L',
-        'lines': this.tableData[47]
+        'column': this.days[this.currentDay].pmEnd,
+        'lines': this.lines
       }
       this.updateSheet(payload)
     },
@@ -95,11 +151,13 @@ export default {
     ])
   },
   mounted() {
+    this.currentDay = moment().format('dddd')
     return [
-      this.amBegin = this.tableData[8],
-      this.amEnd = this.tableData[9],
-      this.pmBegin = this.tableData[10],
-      this.pmEnd = this.tableData[11]
+      this.amBegin = this.tableData[this.days[this.currentDay].amBeginIndex],
+      this.amEnd = this.tableData[this.days[this.currentDay].amEndIndex],
+      this.pmBegin = this.tableData[this.days[this.currentDay].pmBeginIndex],
+      this.pmEnd = this.tableData[this.days[this.currentDay].pmEndIndex],
+      this.lines = this.tableData[47]
     ]
   }
 }
