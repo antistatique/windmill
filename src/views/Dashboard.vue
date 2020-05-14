f16a897680d6abbd24b9ff36f68606620a27fe14
Jérémy Gfeller <36196039+JeremyGfeller@users.noreply.github.com>
2020-05-14 11:23:13 +0200

    Create the vue for the dashboard #12

---
+++ src/views/Dashboard.vue
@@ -0,0 +1,17 @@
+<template>
+   <div class="dashboard">
+  </div> 
+</template>
+<script>
+/* eslint-disable */
+export default {
+  name: 'dashboard',
+  data: () => ({
+	}),
+  methods: {},
+  computed: {
+  },
+  mounted() {
+  }
+}
+</script>
