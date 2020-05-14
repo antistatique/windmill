24d6b80a019900bef5b57f59b0e49f64a5e069b7
Jérémy Gfeller <36196039+JeremyGfeller@users.noreply.github.com>
2020-05-14 11:25:46 +0200

    Get values for a specific user and store values #12
    The firstname Guillaume is going to be changed by an email

--- src/store/dashboard.js
+++ src/store/dashboard.js
@@ -4,11 +4,42 @@
 export default {
   namespaced: true,
   state: {
+    spreadsheetId: process.env.VUE_APP_SPREADSHEET_ID,
+    filteredDataDashboard: null,
   },
   getters: {
+    getDataDashboard: state => state.filteredDataDashboard,
   },
   mutations: {
+    assignDashboardData(state, payload) {
+      state.filteredDataDashboard = payload;
+    }
   },
   actions: {
+    getDashboardSheet({ state, commit }) {
+      var ranges = [ "résumé-2020!A1:M" ];
+      gapi.client.sheets.spreadsheets.values.get({
+        spreadsheetId: state.spreadsheetId,
+        range: ranges
+      }).then((response) => {
+				console.log(response.result)
+        var array = []
+        response.result.values.forEach((element, index) => {
+          if(index > 0){
+						array.push(element)
+          }
+        })
+        array.find(element => {
+					if (element[0] == 'Guillaume') {
+						commit('assignDashboardData', element);
+						return true 
+					}
+        })
+        console.log('Dashboard loaded')
+        if (response.status !== 200) {
+          return false;
+        }
+      });
+    }
   }
 };
