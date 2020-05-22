fba912790ae7fb0862bab4533208afd9e8086dc8
Jérémy Gfeller <36196039+JeremyGfeller@users.noreply.github.com>
2020-05-22 16:39:05 +0200

    Create the design for the dashboard page

--- src/views/Dashboard.vue
+++ src/views/Dashboard.vue
@@ -1,47 +1,81 @@
 <template>
-	<div class="dashboard">
-		<div>{{ result.firstname }}</div>
-		<div>Il te reste {{ result.takeDayCurrentYear }} jours à poser</div>
-		<p>petite phrase</p>
+	<div class="dashboard" v-if="dataLoaded == false">
+		<ErrorPage/>
+	</div>
+	<div class="dashboard" v-else>
 
-		<div>
-			<p>Budget temps</p>
-			<div>
-				Année en cours {{ currentYear }} - {{ result.diffValid }} j
-			</div>
-			<div>
-				Report {{ yearEarlier }} - {{ result.soldeYearEarlier }} j
-			</div>
-			<div>
-				Heures supplémentaires {{ result.overtimeHours }}h
-			</div>
-		</div> <br>
-
-		<div>
-			<p>Consommées</p>
-			<div>
-				Vacances posées {{ result.budgetOffDay }} 
-			</div>
-			<div>
-				Heures supplémentaires réucpérées {{ result.overtimeRecup }} 
-			</div>
-		</div><br>
-
-		<div>
-			<p>Indicatifs</p>
-			<div>
-				Maladie {{ result.sickDay }} 
-			</div>
-			<div>
-				Temps formation {{ result.formationDay }}/5j
-			</div>
-			<div>
-				Budget formation CHF {{ result.moneyFormation }}/1200
-			</div>
+		<div class="header">
+			<span class="icon-right">
+				<BIconBoxArrowRight class="icon icon-logout" @click="logout()"/>
+			</span>
+			<h1 class="username">{{ result.firstname }}</h1>
+			<p class="header-title-bold">Il te reste {{ result.takeDayCurrentYear }} jours à poser</p>
+			<p class="header-subtitle-regular">3 jours de vacances et 1 jour (8.24h) supplémentaire</p>
 		</div>
 
-		<b-button v-b-modal.setLocalStorage class="btn btn-costum">Horaire habituel</b-button>
+		<div class="data-tables">
+			<table class="table-data table-count">
+				<thead>
+					<tr>
+						<th colspan="2">Budget temps</th>
+					</tr>
+				</thead>
+				<tbody>
+					<tr>
+						<td>Année en cours {{ currentYear }}</td>
+						<td>{{ result.diffValid }} j</td>
+					</tr>
+					<tr>
+						<td>Report {{ yearEarlier }}</td>
+						<td>{{ result.soldeYearEarlier }} j</td>
+					</tr>
+					<tr>
+						<td>Heures supplémentaires</td>
+						<td>{{ result.overtimeHours }} h</td>
+					</tr>
+				</tbody>
+			</table>
+			<table class="table-data table-count">
+				<thead>
+					<tr>
+						<th colspan="2">Consommées</th>
+					</tr>
+				</thead>
+				<tbody>
+					<tr>
+						<td>Vacances posées</td>
+						<td>{{ result.budgetOffDay }} j</td>
+					</tr>
+					<tr>
+						<td>Heures supplémentaires réucpérées</td>
+						<td>{{ result.overtimeRecup }}</td>
+					</tr>
+				</tbody>
+			</table>
+			<table class="table-data table-count">
+				<thead>
+					<tr>
+						<th colspan="2">Indicatifs</th>
+					</tr>
+				</thead>
+				<tbody>
+					<tr>
+						<td>Maladie</td>
+						<td>{{ result.sickDay }} j</td>
+					</tr>
+					<tr>
+						<td>Temps formation</td>
+						<td>{{ result.formationDay }}/5 j</td>
+					</tr>
+					<tr>
+						<td>Budget formation CHF</td>
+						<td>{{ result.moneyFormation }}/1200</td>
+					</tr>
+				</tbody>
+			</table>
+		</div>
 
+		<button v-b-modal.setLocalStorage class="button button-primary">Horaire habituel</button>	
 		<b-modal id="setLocalStorage" class="modal">
 			<template v-slot:modal-header="{ cancel }">
 				<h5>Définition de l'horaire habituel</h5>
@@ -194,3 +228,97 @@
 		}
 	}
 </script>
+
+<style>
+	.username {
+		display: inline-block;
+		margin: 0;
+		padding: .55rem .96rem;
+		font-size: 1rem;
+		color: #fff!important;
+		background-color: hsla(0,0%,100%,.19);
+		text-align: center;
+	}
+	.title, .username {
+		border-radius: 4px;
+		font-weight: 600;
+	}
+	h1 {
+		display: block;
+		font-size: 2em;
+		margin-block-start: 0.67em;
+		margin-block-end: 0.67em;
+		margin-inline-start: 0px;
+		margin-inline-end: 0px;
+		font-weight: bold;
+	}
+	.header {
+		background-color: #000032;
+		text-align: center;
+		padding-top: .5rem;
+		padding-bottom: .5rem;
+		opacity: .9;
+		margin: 0;
+	}
+	.header .icon-right {
+		display: -webkit-box;
+		display: -ms-flexbox;
+		display: flex;
+		-webkit-box-pack: end;
+		-ms-flex-pack: end;
+		justify-content: flex-end;
+		margin-right: 1.5rem;
+	}
+	.header .header-title-bold {
+		margin-top: 0;
+		margin-bottom: 0;
+		padding: .5rem 0;
+		font-size: 1.125rem;
+		font-weight: 600;
+		color: #fff;
+	}
+	.header .header-subtitle-regular {
+		margin: 0;
+		font-size: .75rem;
+		font-weight: 400;
+		color: #fff;
+	}
+	.header .icon-right .icon {
+		font-size: 1.25rem;
+		color: #fff;
+	}
+	.data-tables {
+		margin: 1rem;
+	}
+	.table-count {
+		margin-bottom: 1.5rem;
+	}
+	.table-data {
+		width: 100%;
+		border-collapse: collapse;
+	}
+	.table-data thead th {
+		margin-bottom: .5rem;
+		border-radius: 4px;
+		padding: .125rem .5rem;
+		line-height: 1.5rem;
+		color: #000032;
+		background-color: #edf0f3;
+		text-align: left;
+	}
+	.table-data tbody tr {
+		border-bottom: 1px solid #dbe1e7;
+	}
+	.table-count tbody td {
+		padding: .5rem;
+	}
+	.table-count tbody td:last-of-type {
+		font-weight: 600;
+	}
+	.table-data tbody td:first-of-type {
+		text-align: left;
+	}
+	.table-data tbody td:last-of-type {
+		text-align: right;
+	}
+</style>
