c047b2926e249f043a23a3a8c2a8ba10f9c34240
Jérémy Gfeller <36196039+JeremyGfeller@users.noreply.github.com>
2020-05-28 15:50:41 +0200

    Use the font inter #15

--- src/App.vue
+++ src/App.vue
@@ -16,8 +16,9 @@ export default {
 </script>
 
 <style>
+  @import url('https://fonts.googleapis.com/css2?family=Inter&display=swap');
   #app {
-    font-family: Avenir, Helvetica, Arial, sans-serif;
+    font-family: 'Inter', 'Avenir', Helvetica, Arial, sans-serif;
     -webkit-font-smoothing: antialiased;
     -moz-osx-font-smoothing: grayscale;
     color: #2c3e50;
