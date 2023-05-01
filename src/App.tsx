b1fae7595424af57faf95ae3d29e73fb53d28480
Armand Marechal <sypenfu@gmail.com>
2023-05-01 10:41:29 +0200

    Setup Eslint

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,5 @@
-function App() {
-	return (
-		<>
-			<h1 className='text-3xl font-bold underline'>Hello world!</h1>
-		</>
-	);
-}
+const App = () => (
+  <h1 className="text-3xl font-bold underline">Hello world!</h1>
+);
 
 export default App;
