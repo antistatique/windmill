22207cafaa063b6f05a9d89944f9f135237ba62a
Armand Marechal <sypenfu@gmail.com>
2023-05-03 09:24:02 +0200

    Setup Eslint

--- pages/page2.tsx
+++ pages/page2.tsx
@@ -4,7 +4,9 @@ const Page2 = () => {
 	return (
 		<main>
 			<h1 className='text-xl'>Page 2</h1>
-			<button onClick={() => signOut()} className='px-2 bg-pink rounded'>sign out</button>
+			<button onClick={() => signOut()} className='px-2 bg-pink rounded'>
+				sign out
+			</button>
 		</main>
 	);
 };
