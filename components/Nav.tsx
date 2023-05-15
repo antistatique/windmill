75d2eb83bae4f38b0edf9654ea2c47fab1cde6d2
Armand Marechal <sypenfu@gmail.com>
2023-05-15 13:46:35 +0200

    Display active link

--- components/Nav.tsx
+++ components/Nav.tsx
@@ -1,4 +1,5 @@
 import Link from 'next/link';
+import { useRouter } from 'next/router';
 
 import CalendarIcon from '@/components/icons/calendar';
 import DashboardIcon from '@/components/icons/dashboard';
@@ -22,21 +23,29 @@ const routes = [
   },
 ];
 
-const Nav = () => (
-  <nav className="bg-white py-1">
-    <ul className="flex justify-evenly">
-      {routes.map(route => (
-        <li key={route.name}>
-          <Link
-            className="flex h-12 w-12 items-center justify-center text-gray hover:text-pink"
-            href={route.path}
-          >
-            {route.icon}
-          </Link>
-        </li>
-      ))}
-    </ul>
-  </nav>
-);
+const Nav = () => {
+  const router = useRouter();
+
+  return (
+    <nav className="bg-white py-1">
+      <ul className="flex justify-evenly">
+        {routes.map(route => (
+          <li key={route.name}>
+            <Link
+              className={`flex h-12 w-12 items-center justify-center ${
+                router.pathname === route.path
+                  ? 'cursor-default text-blue'
+                  : 'text-gray hover:text-pink'
+              }`}
+              href={route.path}
+            >
+              {route.icon}
+            </Link>
+          </li>
+        ))}
+      </ul>
+    </nav>
+  );
+};
 
 export default Nav;
