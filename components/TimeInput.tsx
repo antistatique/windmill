110ba2d4c504f63acfadca322c3a4c15ef61bfc5
Armand Marechal <sypenfu@gmail.com>
2023-05-15 15:03:33 +0200

    Add button to set current time

--- components/TimeInput.tsx
+++ components/TimeInput.tsx
@@ -1,4 +1,7 @@
 import { MdError } from 'react-icons/md';
+import moment from 'moment';
+
+import ClockIcon from '@/components/icons/clock';
 
 type Props = {
   label: string;
@@ -8,38 +11,54 @@ type Props = {
   onChange: (value: string) => void;
 };
 
-const TimeInput = ({ label, value, disabled, error, onChange }: Props) => (
-  <>
-    <label
-      className={`relative flex w-full items-center justify-center rounded-lg shadow
+const TimeInput = ({ label, value, disabled, error, onChange }: Props) => {
+  const handleSetCurrentTime = () => {
+    if (disabled) return;
+    onChange(moment().format('HH:mm'));
+  };
+
+  return (
+    <>
+      <label
+        className={`flex w-full items-center rounded-lg px-4 shadow
         ${disabled ? 'bg-disabled' : 'cursor-pointer bg-white'}
         ${error ? 'outline outline-3 outline-error' : ''}
       `}
-    >
-      <div
-        data-before={label}
-        className="w-full grow text-center before:absolute before:left-2 before:top-3 before:text-blue before:content-[attr(data-before)]"
       >
-        <input
-          type="time"
-          value={value || '00:00'}
-          onChange={event => onChange(event.target.value)}
-          disabled={disabled}
-          className={`w-[70px] py-3 text-center
+        <label className="pointer-events-none text-blue">{label}</label>
+
+        <div className="w-full grow text-center">
+          <input
+            type="time"
+            value={value || '00:00'}
+            onChange={event => onChange(event.target.value)}
+            disabled={disabled}
+            className={`w-[70px] py-3 text-center
             ${!value ? 'text-gray' : ''}
             ${disabled ? 'bg-disabled' : 'bg-white'}
           `}
-        />
-      </div>
-    </label>
+          />
+        </div>
 
-    {error && (
-      <span className="pb-2 font-normal text-error">
-        <MdError className="mr-2 inline" />
-        {error}
-      </span>
-    )}
-  </>
-);
+        {!disabled && (
+          <button
+            type="button"
+            onClick={handleSetCurrentTime}
+            className="text-gray hover:text-blue"
+          >
+            <ClockIcon  />
+          </button>
+        )}
+      </label>
+
+      {error && (
+        <span className="pb-2 font-normal text-error">
+          <MdError className="mr-2 inline" />
+          {error}
+        </span>
+      )}
+    </>
+  );
+};
 
 export default TimeInput;
