b9f5a1b32b4da70a3d3467a657b3748085c8184c
Armand Marechal <sypenfu@gmail.com>
2023-05-16 10:58:06 +0200

    Setup PWA

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -2,7 +2,11 @@ import { Head, Html, Main, NextScript } from 'next/document';
 
 const Document = () => (
   <Html lang="en">
-    <Head />
+    <Head>
+      <link rel="manifest" href="/manifest.json" />
+      <link rel="apple-touch-icon" href="/icon-192x192.png" />
+      <meta name="theme-color" content="#fff" />
+    </Head>
     <body>
       <Main />
       <NextScript />
