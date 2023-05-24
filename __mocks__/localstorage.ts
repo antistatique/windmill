9d7d5fceb0cb6ecb53a8fac7e2ed957b5b1da1f1
Armand Marechal <sypenfu@gmail.com>
2023-05-24 14:04:21 +0200

    Add unit tests for nudge parameter

---
+++ __mocks__/localstorage.ts
@@ -0,0 +1,14 @@
+const setItemMock = jest.fn();
+const getItemMock = jest.fn();
+
+beforeEach(() => {
+  Storage.prototype.setItem = setItemMock;
+  Storage.prototype.getItem = getItemMock;
+});
+
+afterEach(() => {
+  setItemMock.mockRestore();
+  getItemMock.mockRestore();
+});
+
+export { setItemMock, getItemMock };
