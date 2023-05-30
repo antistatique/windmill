ef0f7b8c1bd5f95d38eb111f6ca5d92f26c25f40
Armand Marechal <sypenfu@gmail.com>
2023-05-30 19:06:13 +0200

    Remove tests on localstorage

--- tests/__mocks__/zustand.ts
+++ tests/__mocks__/zustand.ts
@@ -1,10 +1,8 @@
 import { act } from 'react-dom/test-utils';
 
 const { create: actualCreate } = jest.requireActual('zustand');
-
 // a variable to hold reset functions for all stores declared in the app
 const storeResetFns = new Set();
-
 // when creating a store, we get its initial state, create a reset function and add it in the set
 const create = (createState: any) => {
   const store = actualCreate(createState);
@@ -12,11 +10,9 @@ const create = (createState: any) => {
   storeResetFns.add(() => store.setState(initialState, true));
   return store;
 };
-
 // Reset all stores after each test run
 beforeEach(() => {
   act(() => storeResetFns.forEach((resetFn: any) => resetFn()));
 });
-
 // eslint-disable-next-line import/prefer-default-export
 export { create };
